0769d40a5f791f067b8d5f8fb2ef88a3b8ac7091
HieuDoan11102004 <hieudoan1110@gmail.com>
2024-04-06 23:02:42 +0700

    update show products

--- server/models/productclassify.js
+++ server/models/productclassify.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Product extends Model {
+  class ProductClassify extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,44 +11,35 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Product.belongsTo(models.Productline, {foreignKey: 'productline_id'});
-      Product.hasMany(models.ProductClassify, {foreignKey: 'product_id'});
+      ProductClassify.belongsTo(models.Product, {foreignKey:"product_id"});
     }
   }
-  Product.init({
+  ProductClassify.init({
     id: {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
       type: DataTypes.INTEGER
     },
-    productline_id: {
+    classify_name: {
+      allowNull: false,
+      type: DataTypes.STRING(200)
+    },
+    product_id: {
       type: DataTypes.INTEGER,
       references:{
         model:{
-          tableName:"productlines",
-        },
-        key: 'id'
+          tableName:"products",
+          key:"id"
+        }
       }
     },
-    product_name:{
-      type:DataTypes.STRING(200)
-    },
-    description:{
-      type:DataTypes.STRING(400)
-    },
-    quantity_in_stock:{
-      type:DataTypes.INTEGER
-    },
-    price:{
-      type:DataTypes.DECIMAL
-    }
   }, {
     sequelize,
-    modelName: 'Product',
-    tableName: 'products',
+    modelName: 'ProductClassify',
+    tableName: 'productClassifies',
     createdAt: 'created_at',
     updatedAt: 'updated_at'
   });
-  return Product;
+  return ProductClassify;
 };
