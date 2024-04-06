b792ce1b86274d35151ca02dac7958285f68d7ba
drago467 <tdt.trandinhtuan467@gmail.com>
2024-04-07 00:14:47 +0700

    update getProducts

--- server/controllers/api/v1/products.controller.js
+++ server/controllers/api/v1/products.controller.js
@@ -1,5 +1,4 @@
-const { Product, Productline, ProductClassify } = require("../../../models/index");
-const { Category } = require("../../../models/index");
+const { Product, Productline, ProductClassify, Category } = require("../../../models/index");
 const { errorResponse, successResponse } = require("../../../utils/response");
 
 module.exports = {
@@ -9,13 +8,20 @@ module.exports = {
         include: [
           {
             model: Productline,
+            attributes: ['productline_name'],
             include: [
               {
                 model: Category,
+                attributes: ['category_name'],
               },
             ],
           },
+          {
+            model: ProductClassify,
+            attributes: ['classify_name'],
+          },
         ],
+        attributes: ['product_name', 'description', 'quantity_in_stock', 'price'],
       });
       
       const classify = await ProductClassify.findAll();
@@ -60,76 +66,3 @@ module.exports = {
     }
   },
 };
-
-//   const { Product, sequelize } = require("../../../models/index");
-// const { Category } = require("../../../models/index");
-// const { errorResponse, successResponse } = require("../../../utils/response");
-
-// module.exports = {
-//   showProducts: async (req, res) => {
-//     try {
-//       // const product = await sequelize.query('Select p.*, pl.productline_name, c.category_name from products p join productlines pl on p.productline_id = pl.id join categories c on pl.category_id = c.id');
-//       const product = await Product.findAll({
-//         include: [
-//           { model: Productline, as: 'Productline' },
-//           { model: Category }
-//         ]
-//       });
-//       // const product_productline = await product.getProductLines();
-//       if (!product) {
-//         return errorResponse(res, 404, "Not Found");
-//       } else {
-//         return successResponse(res, 200, "success", product);
-//       }
-//     } catch (e) {
-//       console.log(e);
-//       return errorResponse(res, 500, "Đã có lỗi xảy ra");
-//     }
-//   },
-
-//   showCategories: async(req, res) => {
-//     try {
-//         const category = await Category.findAll();
-//         if (!category) {
-//             return errorResponse(res, 404, "Not Found");
-//         } else {
-//             return successResponse(res, 200, "success", category);
-//         }
-//     } catch (e) {
-//         console.log(e);
-//         return errorResponse(res, 500, "Đã có lỗi xảy ra");
-//     }
-//   }
-// };
-// };
-
-// module.exports = {
-//   getProducts: async (req, res) => {
-//     try {
-//       const products = await Product.findAll({
-//         include: [
-//           {
-//             model: Productline,
-//             as: "productline_id", // Đảm bảo rằng giá trị này khớp với giá trị bạn đã dùng trong phương thức associate của Product
-//             include: [
-//               {
-//                 model: Category,
-//                 as: "category_id", // Đảm bảo rằng giá trị này khớp với giá trị bạn đã dùng trong phương thức associate của Productline
-//               },
-//             ],
-//           },
-//         ],
-//       });
-//       // return successResponse(res, 201, 'Đã lấy sản phẩm thành công', products);
-//       if (!products) {
-//         return errorResponse(res, 404, "Not Found");
-//       } else {
-//         return successResponse(res, 200, "success", products);
-//       }
-//     } catch (error) {
-//       console.error("Error fetching products:", error);
-//       // return errorResponse(res, 404, 'Lấy sản phẩm lỗi')
-//       res.status(500).send("An error occurred while fetching products");
-//     }
-//   },
-// };
